92c15e54042bc923e41f5c9f1fbd98ce074d3a11
benfahrenheit <benfahrenheit@users.noreply.github.com>
2016-01-24 15:20:16 -0500

    Added initial learnyounode workshop, and configured the vscode taskrunner and gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var path = require('path');
+
+function processNodeschoolRunner(runnerCommand, fullFileName) {
+        
+    // Derive the nodeschool runner from the full file name.
+    // We assume the directory name matches the runner.
+    var scriptFile = path.basename(fullFileName);
+    var runnerScript = path.basename(path.dirname(fullFileName));
+    var commandLine = runnerScript + ' ' +
+        runnerCommand + ' ' +
+        scriptFile;
+        
+    console.log('Running ' + commandLine);
+    shell.task(commandLine)();
+}
 
 gulp.task('test', function() {
-    
+    processNodeschoolRunner('run', process.argv[6]);
 });
 
 gulp.task('build', function() {
-    
+    processNodeschoolRunner('verify', process.argv[6]);
 });
