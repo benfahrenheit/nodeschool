6744f4a3154496827936680ba296084e6268acea
benfahrenheit <benfahrenheit@users.noreply.github.com>
2016-02-06 10:14:58 -0500

    Made the books workshop more typescripty. Now with async/await.

--- expressworks/books.ts
+++ expressworks/books.ts
@@ -1,17 +1,28 @@
+'use strict'
+
 import * as express from 'express';
 import * as fs from 'fs';
 
-var booksPath = process.argv[3];
-
-var app = express();
-
-app.get('/books', (req, res) => {
-    fs.readFile(booksPath, (err, data) => {
-        if (!err) {
-            var books = JSON.parse(data.toString());
-            res.json(books);
+// yeah, yeah I know about libs that will promisify stuff for me.
+// but i wanted to try it myself. for learnin' and stuff.
+let readFile = (filename) => {
+  return new Promise<Buffer>((resolve, reject) => {
+     fs.readFile(filename, (err, data) => {
+        if (err) {
+            return reject(err);
         }
-    });
+        resolve(data);
+     });
+  });
+};
+
+let booksPath:string = process.argv[3];
+let app = express();
+
+app.get('/books', async (req, res) => {
+    let data:Buffer = await readFile(booksPath);
+    let booksObj = JSON.parse(data.toString());
+    res.json(booksObj);
 });
 
 app.listen(process.argv[2], () => {
