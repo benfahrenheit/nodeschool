8586d091bdeb6e9be056ca0cae83b65f610fe9d6
Ben <benfahrenheit@users.noreply.github.com>
2016-03-19 19:25:08 -0400

    Exercise 9

---
+++ promise-it-wont-hurt/exercise9.js
@@ -0,0 +1,11 @@
+function parsePromised() {
+    return new Promise((fulfill, reject) => {
+        try {
+            fulfill(JSON.parse(process.argv[2]));
+        } catch(e) {
+            reject(e);
+        }
+    })
+}
+
+parsePromised().then(console.log, console.log);
