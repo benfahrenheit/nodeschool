c368f4055bb969debe7b7d24761ce9ec0becb83c
Ben <benfahrenheit@users.noreply.github.com>
2016-03-19 21:21:02 -0400

    Exercise 10

---
+++ promise-it-wont-hurt/exercise10.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function alwaysThrows() {
+    throw new Error("OH NOES");
+}
+
+function iterate(x) {
+    console.log(x);
+    return x + 1;
+}
+
+Promise.resolve(iterate(1))
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(alwaysThrows)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(null, console.log);
